c07f27bc01fb677875ceae1af54fea15ef013573
Roman Shkabko <33148760+rshkabko@users.noreply.github.com>
2020-05-08 16:59:55 +0300

    Correct mistakes with $checkSum

--- single_kassa.php
+++ single_kassa.php
@@ -1,9 +1,9 @@
 <?php
 $key = 'YOUR_KASSA_SECRET_KEY';
 
-hash('sha256', $_POST['cashboxCode'].'&'.$_POST['orderId'].'&'.$_POST['baseSum'].'&'.$_POST['totalSum'].'&'.$_POST['contact'].'&'.$_POST['paymentCode'].'&'.$_POST['cashboxCurrencyCode'].'&'.$_POST['paymentCurrencyCode'].'&'.$_POST['customInfo'].'&'.$key );
+$checkSum = hash('sha256', $_POST['cashboxCode'].'&'.$_POST['orderId'].'&'.$_POST['baseSum'].'&'.$_POST['totalSum'].'&'.$_POST['contact'].'&'.$_POST['paymentCode'].'&'.$_POST['cashboxCurrencyCode'].'&'.$_POST['paymentCurrencyCode'].'&'.$_POST['customInfo'].'&'.$key );
 //Checking
-if( empty($_POST['sha256_hash']) || $_POST['sha256_hash'] != $sha_keys )
+if( empty($_POST['sha256_hash']) || $_POST['sha256_hash'] != $checkSum )
     exit();
 
 /*
